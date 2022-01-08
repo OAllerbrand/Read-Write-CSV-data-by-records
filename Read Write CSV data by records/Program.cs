a5f9e12bd505d60b5f1f5820e39daf9527c3d80a
Oskar Allerbrand <oallerbrand@gmail.com>
2022-01-08 19:22:05 +0000

    Add project files.

---
+++ Read Write CSV data by records/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Globalization;
+using CsvHelper;
+using CsvHelper.Configuration;
+
+namespace Read_Write_CSV_data_by_records
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+            Console.WriteLine("Successfully installed 'CsvHelper 27.2.1' to Read Write CSV data by records");
+            Console.ReadLine();
+
+            var csvConfig = new CsvConfiguration(CultureInfo.CurrentCulture)
+            {
+                HasHeaderRecord = false
+            };
+
+            using var streamReader = File.OpenText("country_data.csv");
+            using var csvReader = new CsvReader(streamReader, csvConfig);
+
+
+            while (csvReader.Read())
+            {
+                for (int i = 0; csvReader.TryGetField<string>(i, out string value); i++)
+                {
+                    Console.Write($"{value} ");
+                }
+
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
